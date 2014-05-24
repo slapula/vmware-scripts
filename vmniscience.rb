cdcf01fa12c91e93ac82ec24a71e26505f12d6a2
Aaron Smith <ajsmith10381@gmail.com>
2014-05-23 20:37:17 -0500

    Cleaning up and prep'ing for increased functionality

--- vmniscience.rb
+++ vmniscience.rb
@@ -1,11 +1,35 @@
 #!/usr/bin/env ruby
 require 'rbvmomi'
-
+require 'redis'
 
 test = RbVmomi::VIM.connect :host => 'localhost', :port => '14443', :user => '***REMOVED***', :password => '***REMOVED***', :insecure => true
 rootFolder = test.serviceInstance.content.rootFolder
 dc = rootFolder.childEntity.grep(RbVmomi::VIM::Datacenter).find { |x| x.name == "TEST" } or fail "datacenter not found"
+host = dc.hostFolder.children.first.host.first
 
+# ---------------------------------
+# Collects data from Vcenter server
+# ---------------------------------
+
+
+puts host.hardware.memorySize
+puts host.hardware.cpuInfo.numCpuCores
+puts host.summary.runtime.powerState
+puts host.summary.quickStats.overallMemoryUsage
+puts host.summary.quickStats.overallCpuUsage
+puts host.summary.config.name
+
+puts host.summary.config.product.fullName
+puts host.summary.config.product.apiType
+puts host.summary.config.product.apiVersion
+puts host.summary.config.product.osType
+puts host.summary.config.product.productLineId
+puts host.summary.config.product.vendor
+puts host.summary.config.product.version
+
+# ---------------------------------
+# Collects data from individual VMs 
+# ---------------------------------
 def list_folders(df)
 	vms = []
       	df.childEntity.each do |object|
@@ -27,27 +51,4 @@ rootFolder.childEntity.grep(RbVmomi::VIM::Datacenter).each do |dc|
 	break if [:datastore]
 end
 vm
-
-#dc = test.serviceInstance.find_datacenter("TEST") or fail "datacenter not found"
-#host = dc.hostFolder.children.first.host.first
-#folder1 = dc.hostFolder.children.name
-
-host = dc.hostFolder.children.first.host.first
-puts host.hardware.memorySize
-puts host.hardware.cpuInfo.numCpuCores
-puts host.summary.runtime.powerState
-puts host.summary.quickStats.overallMemoryUsage
-puts host.summary.quickStats.overallCpuUsage
-puts host.summary.config.name
-
-puts host.summary.config.product.fullName
-puts host.summary.config.product.apiType
-puts host.summary.config.product.apiVersion
-puts host.summary.config.product.osType
-puts host.summary.config.product.productLineId
-puts host.summary.config.product.vendor
-puts host.summary.config.product.version
-
-#sleep 3
-
 
