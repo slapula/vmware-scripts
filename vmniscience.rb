691f9fd3d1da33bca6e327867c72c3f2cc7e892a
Aaron Smith <ajsmith10381@gmail.com>
2014-05-24 19:06:11 -0500

    Added method to gather data from Host systems -- Host method different from others -- Going to try switching the other methods to this new one

--- vmniscience.rb
+++ vmniscience.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'rbvmomi'
 require 'redis'
+require 'ruby-progressbar'
 
 puts "Updating database...\n"
 
@@ -36,14 +37,52 @@ end
 def collectClusters(rf, db)
 	rf.childEntity.grep(RbVmomi::VIM::Datacenter).each do |dc|
 		foldlist = list_folders(dc.hostFolder)
+		progressbar = ProgressBar.create(:title => "Clusters", :format => '%t |%b>>%i| %p%% %a')
+		progressbar.total = foldlist.count
 		foldlist.each do |hostlist|
 			db.select(1)
-			db.hmset("#{hostlist.name}", "#{hostlist.summary.overallStatus}", "#{hostlist.summary.numHosts}", "#{hostlist.summary.numEffectiveHosts}", "#{hostlist.summary.totalCpu}", "#{hostlist.summary.effectiveCpu}", "#{hostlist.summary.totalMemory}", "#{hostlist.summary.effectiveMemory}")
+			db.hset("#{hostlist.name}", "Status", "#{hostlist.summary.overallStatus}")  
+			db.hset("#{hostlist.name}", "NumberHosts", "#{hostlist.summary.numHosts}")
+			db.hset("#{hostlist.name}", "EffectiveHosts", "#{hostlist.summary.numEffectiveHosts}") 
+			db.hset("#{hostlist.name}", "CPUtotal", "#{hostlist.summary.totalCpu}")
+			db.hset("#{hostlist.name}", "EffectiveCPU", "#{hostlist.summary.effectiveCpu}")
+			db.hset("#{hostlist.name}", "MemTotal", "#{hostlist.summary.totalMemory}")
+			db.hset("#{hostlist.name}", "EffectiveMem", "#{hostlist.summary.effectiveMemory}")
+			progressbar.increment
 		end
 	end
 end
 
-collectClusters(rootFolder, redis)
+#collectClusters(rootFolder, redis)
+
+# ------------------------
+# Collects data from Hosts
+# ------------------------
+
+def collectHosts(rf, db)
+	rf.childEntity.grep(RbVmomi::VIM::Datacenter).each do |dc|
+		count = 0
+		dc.hostFolder.childEntity.each do |cluster|
+			count += cluster.host.count
+		end
+		progressbar = ProgressBar.create(:title => "Hosts", :format => '%t |%b>>%i| %p%% %a')
+		progressbar.total = count
+		dc.hostFolder.childEntity.each do |cluster|
+			cluster.host.each do |host|
+				db.select(2)
+				db.hset("#{host.name}", "Status", "#{host.summary.overallStatus}")
+				db.hset("#{host.name}", "PowerStatus", "#{host.summary.runtime.powerState}")
+				db.hset("#{host.name}", "Connection", "#{host.summary.runtime.connectionState}")
+				db.hset("#{host.name}", "OverallCpu", "#{host.summary.quickStats.overallCpuUsage}")
+				db.hset("#{host.name}", "OverallMem", "#{host.summary.quickStats.overallMemoryUsage}") 
+				#db.hset("#{host.name}", "SystemSensor", "#{host.summary.runtime.healthSystemRuntime.systemHealthInfo.numericSensorInfo.name}")
+				progressbar.increment
+			end
+		end
+	end
+end
+
+collectHosts(rootFolder, redis)
 
 # ---------------------------------
 # Collects data from individual VMs 
@@ -52,9 +91,17 @@ collectClusters(rootFolder, redis)
 def collectVMs(rf, db)
 	rf.childEntity.grep(RbVmomi::VIM::Datacenter).each do |dc|
 		foldlist = list_folders(dc.vmFolder)
+		progressbar = ProgressBar.create(:title => "VMs", :format => '%t |%b>>%i| %p%% %a')
+		progressbar.total = foldlist.count
 		foldlist.each do |vmlist|
-			db.select(2)
-			db.hmset("#{vmlist.name}", "#{vmlist.summary.overallStatus}", "#{vmlist.summary.quickStats.uptimeSeconds}", "#{vmlist.summary.config.numCpu}", "#{vmlist.summary.quickStats.overallCpuUsage}", "#{vmlist.summary.config.memorySizeMB}", "#{vmlist.summary.quickStats.guestMemoryUsage}")
+			db.select(3)
+			db.hset("#{vmlist.name}", "Status", "#{vmlist.summary.overallStatus}")
+			db.hset("#{vmlist.name}", "Uptime", "#{vmlist.summary.quickStats.uptimeSeconds}")
+			db.hset("#{vmlist.name}", "CPUusage", "#{vmlist.summary.quickStats.overallCpuUsage}")
+			db.hset("#{vmlist.name}", "CPUnum", "#{vmlist.summary.config.numCpu}")
+			db.hset("#{vmlist.name}", "MemUsage", "#{vmlist.summary.quickStats.guestMemoryUsage}")
+			db.hset("#{vmlist.name}", "MemTotal", "#{vmlist.summary.config.memorySizeMB}")
+			progressbar.increment
 		end
 	end
 end
